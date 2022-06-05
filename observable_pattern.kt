2e8dd717e02641d1b334169a2da04e2ea7717f01
drabu <burhan.drabu@shadowfax.in>
2022-06-05 13:12:10 +0530

    docs(pattern): Basic Obervale Pattern

---
+++ observable_pattern.kt
@@ -0,0 +1,47 @@
+// https://pl.kotl.in/JpeRYHbwC
+
+/** Observalbe Pattern */
+fun main() {
+    val userOne = User("John Doe", "Joe")
+
+    userOne.observe { it -> print(it) }
+
+    userOne.setNickName("John")
+    userOne.setNickName("Wick")
+}
+
+class User(private var fullName: String, private var nickName: String) : Subject<User> {
+
+    fun setFullName(fullName: String) {
+        this.fullName = fullName
+        notifyObservers()
+    }
+
+    fun setNickName(nickName: String) {
+        this.nickName = nickName
+        notifyObservers()
+    }
+
+    override fun toString() = "NickName : $nickName FullName : $fullName"
+
+    // Obervable Pattern Contents
+    private val observers: ArrayList<Observalbe<User>> = ArrayList()
+
+    override fun observe(observer: Observalbe<User>) = observers.add(observer)
+
+    override fun removeObserver(observer: Observalbe<User>) = observers.remove(observer)
+
+    override fun notifyObservers() {
+        for (observer in observers) observer.update(this)
+    }
+}
+
+interface Subject<T> {
+    fun observe(observer: Observalbe<T>): Boolean
+    fun removeObserver(observer: Observalbe<T>): Boolean
+    fun notifyObservers()
+}
+
+fun interface Observalbe<T> {
+    fun update(any: T)
+}
