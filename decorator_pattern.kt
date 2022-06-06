2a0dddf7d2e79fb12b451852b22fb38597726ee8
drabu <burhan.drabu@shadowfax.in>
2022-06-07 00:21:00 +0530

    docs(pattern): Decorator Pattern

---
+++ decorator_pattern.kt
@@ -0,0 +1,45 @@
+// Decorator Patern
+// https://pl.kotl.in/h1AIwrE77
+
+fun main() {
+
+    var myStyleOfBrew: Bewrage = DarkRoast()
+    println(myStyleOfBrew.cost())
+    myStyleOfBrew = Moca(myStyleOfBrew)
+    myStyleOfBrew = Moca(myStyleOfBrew)
+    myStyleOfBrew = Whip(myStyleOfBrew)
+
+    println(myStyleOfBrew.cost())
+}
+
+class Moca(brew: Bewrage) : Contindment(brew) {
+    override fun cost() = bewrage.cost() + 1
+}
+
+class Whip(brew: Bewrage) : Contindment(brew) {
+    override fun cost() = bewrage.cost() + 9
+}
+
+abstract class Contindment(val bewrage: Bewrage) : Bewrage() {}
+
+abstract class Bewrage {
+    private val description: String = "Unknown description"
+    fun getDescription() = description
+    abstract fun cost(): Int
+}
+
+class HouseBlend : Bewrage() {
+    override fun cost() = 22
+}
+
+class DarkRoast : Bewrage() {
+    override fun cost() = 7
+}
+
+class Decaf : Bewrage() {
+    override fun cost() = 10
+}
+
+class Espresso : Bewrage() {
+    override fun cost() = 12
+}
